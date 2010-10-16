21ba76cd781d17622908f7ce3b8e0355abff1d37
andrzej-aa <andrzej.aa@gmail.com>
2010-10-16 23:00:51 +0200

    updated date formatting (that's because of mistake which was taken from http://github.com/themouette/jquery-week-calendar/issues#issue/13)

--- jquery.weekcalendar.js
+++ jquery.weekcalendar.js
@@ -1990,141 +1990,72 @@
        * http://jacwright.com/projects/javascript/date_format
        */
       _formatDate : function(date, format) {
-         var options = this.options;
-         var returnStr = '';
-         for(var i = 0; i < format.length; i++) {
-            var curChar = format.charAt(i);
-            if ($.isFunction(this._replaceChars[curChar])) {
-            //   returnStr += this._replaceChars[curChar](date, options);
-              var res = this._replaceChars[curChar](date, options);
-              if (res === '00' && options.alwaysDisplayTimeMinutes === false) {
-                 //remove previous character
-                 returnStr = returnStr.slice(0, -1);
-              }
-              else {
-                    returnStr += res;    
-              }
-            } else {
-               returnStr += curChar;
-            }
-         }
-         return returnStr;
+			var returnStr = '';
+			for (var i = 0; i < format.length; i++) {
+				var curChar = format.charAt(i);
+				if (i != 0 && format.charAt(i - 1) == "\\") { 
+					returnStr += curChar;
+				}
+				else if (this._replaceChars[curChar]) {
+					returnStr += this._replaceChars[curChar](date, this._formatDate);
+				} else if (curChar != "\\"){
+					returnStr += curChar;
+				}
+			}
+			return returnStr;
       },
 
       _replaceChars : {
-
-         // Day
-         d: function(date) {
-            return (date.getDate() < 10 ? '0' : '') + date.getDate();
-         },
-         D: function(date, options) {
-            return options.shortDays[date.getDay()];
-         },
-         j: function(date) {
-            return date.getDate();
-         },
-         l: function(date, options) {
-            return options.longDays[date.getDay()];
-         },
-         N: function(date) {
-            return date.getDay() + 1;
-         },
-         S: function(date) {
-            return (date.getDate() % 10 == 1 && date.getDate() != 11 ? 'st' : (date.getDate() % 10 == 2 && date.getDate() != 12 ? 'nd' : (date.getDate() % 10 == 3 && date.getDate() != 13 ? 'rd' : 'th')));
-         },
-         w: function(date) {
-            return date.getDay();
-         },
-         z: function(date) {
-            return "Not Yet Supported";
-         },
-         // Week
-         W: function(date) {
-            return "Not Yet Supported";
-         },
-         // Month
-         F: function(date, options) {
-            return options.longMonths[date.getMonth()];
-         },
-         m: function(date) {
-            return (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1);
-         },
-         M: function(date, options) {
-            return options.shortMonths[date.getMonth()];
-         },
-         n: function(date) {
-            return date.getMonth() + 1;
-         },
-         t: function(date) {
-            return "Not Yet Supported";
-         },
-         // Year
-         L: function(date) {
-            return "Not Yet Supported";
-         },
-         o: function(date) {
-            return "Not Supported";
-         },
-         Y: function(date) {
-            return date.getFullYear();
-         },
-         y: function(date) {
-            return ('' + date.getFullYear()).substr(2);
-         },
-         // Time
-         a: function(date) {
-            return date.getHours() < 12 ? 'am' : 'pm';
-         },
-         A: function(date) {
-            return date.getHours() < 12 ? 'AM' : 'PM';
-         },
-         B: function(date) {
-            return "Not Yet Supported";
-         },
-         g: function(date) {
-            return date.getHours() % 12 || 12;
-         },
-         G: function(date) {
-            return date.getHours();
-         },
-         h: function(date) {
-            return ((date.getHours() % 12 || 12) < 10 ? '0' : '') + (date.getHours() % 12 || 12);
-         },
-         H: function(date) {
-            return (date.getHours() < 10 ? '0' : '') + date.getHours();
-         },
-         i: function(date) {
-            return (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-         },
-         s: function(date) {
-            return (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
-         },
-         // Timezone
-         e: function(date) {
-            return "Not Yet Supported";
-         },
-         I: function(date) {
-            return "Not Supported";
-         },
-         O: function(date) {
-            return (date.getTimezoneOffset() < 0 ? '-' : '+') + (date.getTimezoneOffset() / 60 < 10 ? '0' : '') + (date.getTimezoneOffset() / 60) + '00';
-         },
-         T: function(date) {
-            return "Not Yet Supported";
-         },
-         Z: function(date) {
-            return date.getTimezoneOffset() * 60;
-         },
-         // Full Date/Time
-         c: function(date) {
-            return "Not Yet Supported";
-         },
-         r: function(date) {
-            return date.toString();
-         },
-         U: function(date) {
-            return date.getTime() / 1000;
-         }
+			shortMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+			longMonths: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+			shortDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+			longDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+			
+			// Day
+			d: function(date) { return (date.getDate() < 10 ? '0' : '') + date.getDate(); },
+			D: function(date) { return this.shortDays[date.getDay()]; },
+			j: function(date) { return date.getDate(); },
+			l: function(date) { return this.longDays[date.getDay()]; },
+			N: function(date) { return date.getDay() + 1; },
+			S: function(date) { return (date.getDate() % 10 == 1 && date.getDate() != 11 ? 'st' : (date.getDate() % 10 == 2 && date.getDate() != 12 ? 'nd' : (date.getDate() % 10 == 3 && date.getDate() != 13 ? 'rd' : 'th'))); },
+			w: function(date) { return date.getDay(); },
+			z: function(date) { var d = new Date(date.getFullYear(),0,1); return Math.ceil((date - d) / 86400000); }, // Fixed now
+			// Week
+			W: function(date) { var d = new Date(date.getFullYear(), 0, 1); return Math.ceil((((date - d) / 86400000) + d.getDay() + 1) / 7); }, // Fixed now
+			// Month
+			F: function(date) { return this.longMonths[date.getMonth()]; },
+			m: function(date) { return (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1); },
+			M: function(date) { return this.shortMonths[date.getMonth()]; },
+			n: function(date) { return date.getMonth() + 1; },
+			t: function(date) { var d = new Date(); return new Date(d.getFullYear(), d.getMonth(), 0).getDate() }, // Fixed now, gets #days of date
+			// Year
+			L: function(date) { var year = date.getFullYear(); return (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)); },	// Fixed now
+			o: function(date) { var d  = new Date(date.valueOf());  d.setDate(d.getDate() - ((date.getDay() + 6) % 7) + 3); return d.getFullYear();}, //Fixed now
+			Y: function(date) { return date.getFullYear(); },
+			y: function(date) { return ('' + date.getFullYear()).substr(2); },
+			// Time
+			a: function(date) { return date.getHours() < 12 ? 'am' : 'pm'; },
+			A: function(date) { return date.getHours() < 12 ? 'AM' : 'PM'; },
+			B: function(date) { return Math.floor((((date.getUTCHours() + 1) % 24) + date.getUTCMinutes() / 60 + date.getUTCSeconds() / 3600) * 1000 / 24); }, // Fixed now
+			g: function(date) { return date.getHours() % 12 || 12; },
+			G: function(date) { return date.getHours(); },
+			h: function(date) { return ((date.getHours() % 12 || 12) < 10 ? '0' : '') + (date.getHours() % 12 || 12); },
+			H: function(date) { return (date.getHours() < 10 ? '0' : '') + date.getHours(); },
+			i: function(date) { return (date.getMinutes() < 10 ? '0' : '') + date.getMinutes(); },
+			s: function(date) { return (date.getSeconds() < 10 ? '0' : '') + date.getSeconds(); },
+			u: function(date) { var m = date.getMilliseconds(); return (m < 10 ? '00' : (m < 100 ?
+		'0' : '')) + m; },
+			// Timezone
+			e: function(date) { return "Not Yet Supported"; },
+			I: function(date) { return "Not Yet Supported"; },
+			O: function(date) { return (-date.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(date.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(date.getTimezoneOffset() / 60)) + '00'; },
+			P: function(date) { return (-date.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(date.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(date.getTimezoneOffset() / 60)) + ':00'; }, // Fixed now
+			T: function(date) { var m = date.getMonth(); date.setMonth(0); var result = date.toTimeString().replace(/^.+ \(?([^\)]+)\)?$/, '$1'); date.setMonth(m); return result;},
+			Z: function(date) { return -date.getTimezoneOffset() * 60; },
+			// Full Date/Time
+			c: function(date, _formatDate) { return _formatDate("Y-m-d\\TH:i:sP"); }, // Fixed now
+			r: function(date) { return date.toString(); },
+			U: function(date) { return date.getTime() / 1000; }
       },
       
       /* USER MANAGEMENT FUNCTIONS */
